f95a44f1440586be307c4ec7e40dc8997eb5fded
muyouqing <liuq915@gmail.com>
2026-05-09 16:13:33 -0700

    delete the useless files

--- ParkingLot/ParkingLot.java
+++ ParkingLot/ParkingLot.java
@@ -9,14 +9,10 @@ public class ParkingLot {
 
     public ParkingLot(List<Level> levels) {
         this.levels = levels;
-        this.vehicleSizeMap = new HashMap<>();
-        vehicleSizeMap.put(VehicleType.Sadan, 1);
-        vehicleSizeMap.put(VehicleType.SUV, 2);
-        vehicleSizeMap.put(VehicleType.Truck, 3);
     }
     public Ticket park(Vehicle vehicle, boolean needHandicapped) {
         boolean needElectric = vehicle.isElectric();
-        int size = vehicleSizeMap.get(vehicle.getVehicleType());
+        int size = vehicle.getSize();
         for (Level level : levels) {
             for (ParkingSlot slot : level.getParkingSlots()) {
                 //handicapped slot
@@ -49,14 +45,7 @@ public class ParkingLot {
     }
     private double calculatePrice(Ticket ticket) {
         long duration = Instant.now().toEpochMilli() - ticket.createdAt.toEpochMilli();
-        double pricePerHour = 0;
-        if (ticket.vehicle instanceof Sadan) {
-            pricePerHour = 2.0;
-        } else if (ticket.vehicle instanceof SUV) {
-            pricePerHour = 3.0;
-        } else if (ticket.vehicle instanceof Truck) {
-            pricePerHour = 4.0;
-        }
+        double pricePerHour = ticket.vehicle.getPrice();
         return (duration / (1000 * 60 * 60)) * pricePerHour;
     }
 }
